f66ce2b3733b1eb0f2bd267ecce1c53a85773aea
AlfredVilorio <143658419+AlfredVilorio@users.noreply.github.com>
2023-09-23 15:52:04 +0000

    Added some pseudocode

--- main.cs
+++ main.cs
@@ -2,17 +2,20 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-   int twc = 140;
-
+   int twc = 140; // Maximum wordcount//
+    
+    //Input message//
     Console.WriteLine("Enter a message: ");
     string wordcountinput = Console.ReadLine();
 
+    //If wordcount does not exceed it is posted//
      if(wordcountinput.Length < twc)
     
       {
         Console.WriteLine("Posted");
         }
 
+    //If wordcount exceeds, it is not posted//
     else{
 
         Console.WriteLine("The message exceeds wordcount");
